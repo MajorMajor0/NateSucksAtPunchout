4ff3fcf0f84e9e07554a28f584f42daf927ee944
Major Major <mtpogue@gmail.com>
2022-01-09 17:15:40 -0800

    Migrate to .net 6

--- NateSucksAtPunchout/DataModel.Extensions/Player.Extensions.cs
+++ NateSucksAtPunchout/DataModel.Extensions/Player.Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using ChartJs.Blazor.ChartJS.Common;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,6 +38,7 @@ namespace NateSucksAtPunchout.DataModel
 		/// <summary>
 		/// Add up all winnings through year normalized to some number
 		/// </summary>
+		/// <param name="normalizedTo">Number to normalize to</param>
 		/// <returns>Returns an array where entry i is the cumulative winning total up through year i. Subtracts out buy-in</returns>
 		public List<Point> GetNormalizedCumulativeWinnings(double normalizedTo)
 		{
@@ -46,16 +48,85 @@ namespace NateSucksAtPunchout.DataModel
 
 			List<Point> points = new List<Point>();
 
+			// Calculate normalized total for each year
 			for (int i = 0; i < n; i++)
 			{
+				// X axis is the year
 				Point point = new Point { X = finishes[i].Year };
 
+				// Run over years up to and including year i
 				for (int j = 0; j <= i; j++)
 				{
-					point.Y += finishes[j].Amount - finishes[j].YearNavigation.BuyIn;
+					// Add the winnings in each year, normalized to the amount provided
+					point.Y += (finishes[j].Amount - finishes[j].YearNavigation.BuyIn)
+						* normalizedTo / finishes[j].YearNavigation.BuyIn;
 				}
 
-				point.Y = point.Y * normalizedTo / finishes[i].YearNavigation.BuyIn;
+				points.Add(point);
+			}
+
+			return points;
+		}
+
+		/// <summary>
+		/// Add up all cumulative wins to year
+		/// </summary>
+		/// <returns>Returns an array where entry i is the cumulative number of wins up through year i. Subtracts out buy-in</returns>
+		public List<Point> GetWins()
+		{
+			int n = Finishes.Count;
+
+			var finishes = Finishes.ToArray();
+
+			List<Point> points = new List<Point>();
+
+			// Calculate normalized total for each year
+			for (int i = 0; i < n; i++)
+			{
+				// X axis is the year
+				Point point = new Point { X = finishes[i].Year };
+
+				// Run over years up to and including year i
+				for (int j = 0; j <= i; j++)
+				{
+					// Add the winnings in each year, normalized to the amount provided
+					if (finishes[j].Place == 1)
+					{
+						point.Y++;
+					}
+				}
+
+				points.Add(point);
+			}
+
+			return points;
+		}
+
+		/// <summary>
+		/// Add up all hours played through year normalized to some number
+		/// </summary>
+		/// <param name="normalizedTo">Number to normalize to</param>
+		/// <returns>Returns an array where entry i is the cumulative winning total up through year i. Subtracts out buy-in</returns>
+		public List<Point> GetNormalizedCumulativeHours(double normalizedTo)
+		{
+			int n = Finishes.Count;
+
+			var finishes = Finishes.ToArray();
+
+			List<Point> points = new List<Point>();
+
+			// Calculate normalized total for each year
+			for (int i = 0; i < n; i++)
+			{
+				// X axis is the year
+				Point point = new Point { X = finishes[i].Year };
+
+				// Run over years up to and including year i
+				for (int j = 0; j <= i; j++)
+				{
+					// Add the winnings in each year, normalized to the amount provided
+					point.Y += finishes[j].Time * normalizedTo / finishes[j].YearNavigation.Length;
+				}
 
 				points.Add(point);
 			}
--- NateSucksAtPunchout/Pages/Charts.Razor.cs
+++ NateSucksAtPunchout/Pages/Charts.Razor.cs
@@ -15,6 +15,7 @@ namespace NateSucksAtPunchout.Pages
 {
 	public partial class Charts
 	{
+		private int pointRadius = 3;
 		/// <summary>
 		/// Go out to the database on load the cumulative winnings chart information
 		/// </summary>
@@ -54,12 +55,74 @@ namespace NateSucksAtPunchout.Pages
 					BorderColor = player.Color,
 					ShowLine = true,
 					LineTension = 0,
-					PointRadius = 5,
+					PointRadius = pointRadius,
 					PointHitRadius = 5,
 					PointHoverRadius = 8
 				};
 				sds.Label = player.FirstName;
 				sds.Data = player.GetNormalizedCumulativeWinnings(normalizedTo);
+				returner.Add(sds);
+			}
+
+			return returner;
+		}
+
+		/// <summary>
+		/// Go out to the database on load the wins chart information
+		/// </summary>
+		/// <param name="context"></param>
+		/// <returns></returns>
+		private List<ScatterDataset> GetWinsDataSets(BigGameStatsContext context)
+		{
+			List<ScatterDataset> returner = new List<ScatterDataset>();
+			var players = context.Players.Include(x => x.Finishes).ThenInclude(x => x.YearNavigation).ToList();
+
+			foreach (Player player in players)
+			{
+				string color = ColorUtil.RandomColorString();
+				ScatterDataset sds = new ScatterDataset
+				{
+					BackgroundColor = player.Color,
+					BorderColor = player.Color,
+					ShowLine = true,
+					LineTension = 0,
+					PointRadius = pointRadius,
+					PointHitRadius = 5,
+					PointHoverRadius = 8
+				};
+				sds.Label = player.FirstName;
+				sds.Data = player.GetWins();
+				returner.Add(sds);
+			}
+
+			return returner;
+		}
+
+		/// <summary>
+		/// Go out to the database on load the normal winnings chart information
+		/// </summary>
+		/// <param name="context"></param>
+		/// <returns></returns>
+		private List<ScatterDataset> GetNormalizedCumulativeHoursDataSets(BigGameStatsContext context, double normalizedTo)
+		{
+			List<ScatterDataset> returner = new List<ScatterDataset>();
+			var players = context.Players.Include(x => x.Finishes).ThenInclude(x => x.YearNavigation).ToList();
+
+			foreach (Player player in players)
+			{
+				string color = ColorUtil.RandomColorString();
+				ScatterDataset sds = new ScatterDataset
+				{
+					BackgroundColor = player.Color,
+					BorderColor = player.Color,
+					ShowLine = true,
+					LineTension = 0,
+					PointRadius = pointRadius,
+					PointHitRadius = 5,
+					PointHoverRadius = 8
+				};
+				sds.Label = player.FirstName;
+				sds.Data = player.GetNormalizedCumulativeHours(normalizedTo);
 				returner.Add(sds);
 			}
 
